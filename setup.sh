f908bd0515c5467ce59a96ffb6a1a34cc25b46b8
sivakiran9 <47302042+sivakiran9@users.noreply.github.com>
2019-11-20 14:41:47 +0530

    setup

--- setup.sh
+++ setup.sh
@@ -1,66 +1,66 @@
 #!/bin/bash
 
-#echo updating
+echo updating
 apt-get update
 apt-get install -y git
-#echo ssh server is installing
-#apt-get install -y openssh-server
-#echo installing the wget command
-#apt-get install -y wget
-#echo git installing is done
-#apt-get install -y sudo
+echo ssh server is installing
+apt-get install -y openssh-server
+echo installing the wget command
+apt-get install -y wget
+echo git installing is done
+apt-get install -y sudo
 
-#useradd -m -d /home/satya -s /bin/bash satya
-#echo "satya:satya" | chpasswd
-#sed -i '21i satya    ALL=(ALL:ALL) ALL' /etc/sudoers
+useradd -m -d /home/satya -s /bin/bash satya
+echo "satya:satya" | chpasswd
+sed -i '21i satya    ALL=(ALL:ALL) ALL' /etc/sudoers
 
-#cd /home/satya
+cd /home/satya
 
-#mkdir distros
-#chmod -R 777 distros
-#cd distros
+mkdir distros
+chmod -R 777 distros
+cd distros
 
-#echo download the java
-#wget jdk-8uversion-linux-x64.tar.gz
-#tar -zxvf jdk-8uversion-linux-x64.tar.gz
+echo download the java
+wget jdk-8uversion-linux-x64.tar.gz
+tar -zxvf jdk-8uversion-linux-x64.tar.gz
 
-#echo Download the java
-#wget https://download.oracle.com/otn/java/jdk/8u231-b11/5b13a193868b4bf28bcb45c792fce896/jdk-8u231-linux-x64.tar.gz
-#tar -zxvf jdk-8u231-linux-x64.tar.gz
+echo Download the java
+wget https://download.oracle.com/otn/java/jdk/8u231-b11/5b13a193868b4bf28bcb45c792fce896/jdk-8u231-linux-x64.tar.gz
+tar -zxvf jdk-8u231-linux-x64.tar.gz
 
-#echo Download jdk
+echo Download jdk
 
-#wget --no-check-certificate --no-cookies --header "Cookie: oraclelicense=accept-securebackup-cookie" http://download.oracle.com/otn-pub/java/jdk/8u141-b15/336fa29ff2bb4ef291e347e091f7f4a7/jdk-8u141-linux-x64.tar.gz
-#tar zxpvf jdk-8u141-linux-x64.tar.gz
+wget --no-check-certificate --no-cookies --header "Cookie: oraclelicense=accept-securebackup-cookie" http://download.oracle.com/otn-pub/java/jdk/8u141-b15/336fa29ff2bb4ef291e347e091f7f4a7/jdk-8u141-linux-x64.tar.gz
+tar zxpvf jdk-8u141-linux-x64.tar.gz
 
-#echo Download the maven
+echo Download the maven
  
-#wget http://mirrors.estointernet.in/apache/maven/maven-3/3.6.2/binaries/apache-maven-3.6.2-bin.tar.gz
+wget http://mirrors.estointernet.in/apache/maven/maven-3/3.6.2/binaries/apache-maven-3.6.2-bin.tar.gz
 
-#echo extract the file
+echo extract the file
 
-#tar -zxvf apache-maven-3.6.2-bin.tar.gz
+tar -zxvf apache-maven-3.6.2-bin.tar.gz
 
-#echo Download the Tomcat8
+echo Download the Tomcat8
 
-#wget http://apachemirror.wuchna.com/tomcat/tomcat-8/v8.5.47/bin/apache-tomcat-8.5.47.tar.gz
+wget http://apachemirror.wuchna.com/tomcat/tomcat-8/v8.5.47/bin/apache-tomcat-8.5.47.tar.gz
 
-#echo extract the file
+echo extract the file
 
-#tar -zxvf apache-tomcat-8.5.47.tar.gz
+tar -zxvf apache-tomcat-8.5.47.tar.gz
 
-#echo setup the environment variables
+echo setup the environment variables
 
-#echo 'export JAVA_HOME=/home/satya/distros/jdk1.8.0_141
-#      export CATALINA_HOME=/home/satya/distros/apache-tomcat-8.5.47
-#      export M2_HOME=/home/satya/distros/apache-maven-3.6.2
-#      export PATH=$JAVA_HOME/bin:$CATALINA_HOME/bin:$M2_HOME/bin:$PATH' >> /root/.bashrc
+echo 'export JAVA_HOME=/home/satya/distros/jdk1.8.0_141
+      export CATALINA_HOME=/home/satya/distros/apache-tomcat-8.5.47
+      export M2_HOME=/home/satya/distros/apache-maven-3.6.2
+      export PATH=$JAVA_HOME/bin:$CATALINA_HOME/bin:$M2_HOME/bin:$PATH' >> /root/.bashrc
 
-#cd 
+cd 
 
-#source .bashrc
+source .bashrc
 
-#echo source .bashrc is done
+echo source .bashrc is done
 
 echo Download the jenkins war file 
 
